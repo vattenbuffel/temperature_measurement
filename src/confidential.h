da4f56e84a4d786cb8fa4153b221afbb30f3f255
Noa <noal@student.chalmers.se>
2021-09-01 15:13:26 +0200

    Before laptop crash

--- src/confidential.h
+++ src/confidential.h
@@ -4,9 +4,11 @@
 /*#define WIFI_SSID "Noa 2.5 GHz"
 #define WIFI_PASSWORD "rumpnisse"
 #define WIFI_SSID "Noa"
-#define WIFI_PASSWORD "hejhejhej"*/
+#define WIFI_PASSWORD "hejhejhej"
 #define WIFI_SSID "4G-Gateway-5002"
-#define WIFI_PASSWORD "Emanuelsson123"
+#define WIFI_PASSWORD "Emanuelsson123"*/
+#define WIFI_SSID "Lilla huset"
+#define WIFI_PASSWORD "Vera1998"
 #define MQTT_BROKER "192.168.8.11"
 
 #endif //CONFIDENTIAL_H_
